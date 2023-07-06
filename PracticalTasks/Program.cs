fa2b2531119d7eb8783328b547a4c419b468136f
Asrorjon Kodirov <asrorjon.kodirov@techcells.com>
2023-07-06 22:27:07 +0500

    Development and build tools

--- PracticalTasks/Program.cs
+++ PracticalTasks/Program.cs
@@ -1,1 +1,33 @@
-﻿
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the string: ");
+        string str = Console.ReadLine();
+
+        int maxCount = 1;
+        int currentCount = 1;
+        char previousChar = str[0];
+
+        for (int i = 1; i < str.Length; i++)
+        {
+            char currentChar = str[i];
+
+            if (currentChar != previousChar)
+            {
+                currentCount++;
+                maxCount = Math.Max(maxCount, currentCount);
+            }
+            else
+            {
+                currentCount = 1;
+            }
+
+            previousChar = currentChar;
+        }
+
+        Console.WriteLine($"The maximum number of unequal consecutive characters is {maxCount}.");
+    }
+}
