a74007d7109d5a9413e71c37c9b00832257ed946
Asrorjon Kodirov <asrorjon.kodirov@techcells.com>
2023-07-06 16:59:20 +0500

    Basic of .NET platform and C#

--- PracticalTasks/Program.cs
+++ PracticalTasks/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int dec;
+int baseNum;
+
+Console.WriteLine("Enter decimal number: ");
+dec = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter base number: ");
+baseNum = Convert.ToInt32(Console.ReadLine());
+
+string digits = "0123456789ABCDEFGHIJ";
+string ans = "";
+
+while(dec > 0)
+{
+    int remainder = dec % baseNum;
+
+    ans = digits[remainder] + ans;
+
+    dec /= baseNum;
+}
+
+Console.WriteLine(ans);
